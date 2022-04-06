f21d3f6be67e813f3b7071e14250b414e5b81960
Sunny Parikh <SParikh@arbormemorial.com>
2022-04-06 11:48:39 -0400

    Bar with content projection with custom directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
 import { CdkDragStart } from '@angular/cdk/drag-drop';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { Component, Directive, Input, TemplateRef, ContentChild, HostBinding, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -34,4 +34,37 @@ export class AppComponent {
     this.showSubContainer = !this.showSubContainer;
     this.statusClass = 'collapse';
   }
+}
+
+//------------------------------New Action Bar Code-----------------------------------
+
+@Directive({
+  selector: 'button[appNewactionBarToggle]',
+})
+export class NewActionBarToggleDirective {
+  @HostBinding('attr.aria-expanded') ariaExpanded = this.newActionBar.expanded;
+  @HostBinding('attr.aria-controls') ariaControls = this.newActionBar.contentId;
+  @HostListener('click') toggleNewActionBar() {
+    this.newActionBar.expanded = !this.newActionBar.expanded;
+  }
+  constructor(public newActionBar: NewActionBarComponent) {}
+}
+
+@Directive({
+  selector: '[appNewActionBarContent]'
+})
+export class NewactionBarContentDirective {
+  constructor(public templateRef: TemplateRef<unknown>) {}
+}
+
+let nextId = 0;
+
+@Component({
+  selector: 'app-new-action-bar',
+  templateUrl: 'new-action-bar.template.html',
+})
+export class NewActionBarComponent {
+  contentId = `newActionBar-${nextId++}`;
+  @Input() expanded = false;
+  @ContentChild(NewactionBarContentDirective) content!: NewactionBarContentDirective;
 }
